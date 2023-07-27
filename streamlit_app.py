e2fc6619e20b5bee194012ab1b67b1801e738075
Michał Piekarski <michal.piekarski@icloud.com>
2023-07-27 19:07:25 +0200

    Update streamlit_app.py
    Extracted functions for adding new fruits to the load list
    Reenabled all app features

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,12 +55,15 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
 streamlit.header("Add fruit to the load list")
-add_fruit_choice = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thank for adding ', add_fruit_choice)
 
-# This will not work corrently, but just go with it for now
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values('from streamlit')")
+        return 'Thank for adding ' + new_fruit
+
+add_fruit_choice = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_fruit_choice)
+    streamlit.text(back_from_function)
